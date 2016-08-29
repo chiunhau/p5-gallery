ec3bea247bbc3f59040fc1cb89ccf458675760a7
Chiun Hau You <chiunhau9@gmail.com>
2016-08-29 20:51:17 +0800

    so cool

--- circle-forms-2/sketch.js
+++ circle-forms-2/sketch.js
@@ -6,20 +6,20 @@ function setup() {
 }
 
 function draw() {
-  // background(255, 0, 0);
+  background(255, 0, 0);
   translate(width / 2, height / 2);
   drawCircle(Math.sin(frameCount * 0.001));
 }
 
 function drawCircle(s) {
-  for (var i = 0; i < 150; i ++) {
+  for (var i = 0; i < 100; i ++) {
     var hue = color(Math.sin(s * i * 0.1) * 50 + 50, 100, 50);
     fill(hue, 100, 60);
     noStroke();
     push();
-    rotate(i * 2 * PI / 75 + frameCount * 0.01);
+    rotate(i * 2 * PI / 50 + frameCount * 0.01);
     var w = Math.floor(Math.tan(i * s) * 200 + 300);
-    rect(0, 0, w, 20);
+    rect(0, 0, w, 10);
     pop();
   }
 }
