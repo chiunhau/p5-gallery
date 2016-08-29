ad754885d6ba783a6bfd06f770012f1446308cd9
Chiun Hau You <chiunhau9@gmail.com>
2016-08-29 21:31:20 +0800

    circle 3

--- circle-forms-3/sketch.js
+++ circle-forms-3/sketch.js
@@ -6,20 +6,20 @@ function setup() {
 }
 
 function draw() {
-  // background(255, 0, 0);
+  background(255, 0, 0);
   translate(width / 2, height / 2);
-  drawCircle(Math.sin(frameCount * 0.0008));
+  drawCircle((Math.cos(frameCount * 0.01) + 1) * 0.009);
 }
 
 function drawCircle(s) {
-  for (var i = 0; i < 150; i ++) {
+  for (var i = 0; i < 1000; i ++) {
     var hue = color(Math.sin(s * i * 0.1) * 50 + 50, 100, 50);
     fill(hue, 100, 60);
     noStroke();
     push();
-    rotate(i * 2 * PI / 75 - frameCount * 0.01);
-    var w = Math.floor(Math.tan(i * s) * 150 + 200);
-    rect(0, 0, w, 20);
+    rotate(i * 2 * PI / 1000);
+    var w = Math.floor(Math.cos(i * s) * 200 + 400);
+    rect(0, 0, w, 2);
     pop();
   }
 }
