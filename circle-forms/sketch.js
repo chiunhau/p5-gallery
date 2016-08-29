8e2f243e8c556aeb09ccd42c470f716a001cc2d8
Chiun Hau You <chiunhau9@gmail.com>
2016-08-29 20:33:07 +0800

    add many many

---
+++ circle-forms/sketch.js
@@ -0,0 +1,25 @@
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+  rectMode(CENTER);
+  colorMode(HSL, 100);
+  background(255, 0, 0);
+}
+
+function draw() {
+  // background(255, 0, 0);
+  translate(width / 2, height / 2);
+  drawCircle(Math.sin(frameCount * 0.001));
+}
+
+function drawCircle(s) {
+  for (var i = 0; i < 150; i ++) {
+    var hue = color(Math.sin(s * i * 0.1) * 50 + 50, 100, 50);
+    fill(hue, 100, 60);
+    noStroke();
+    push();
+    rotate(i * 2 * PI / 50 + frameCount * 0.01);
+    var w = Math.floor(Math.tan(i * s) * 200 + 300);
+    rect(0, 0, w, 10);
+    pop();
+  }
+}
---
+++ cross-grid-2/sketch.js
@@ -0,0 +1,63 @@
+var grid_size = 25;
+var bar_width = 10;
+var colorPool;
+
+
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+  rectMode(CENTER);
+  background(50);
+  colorMode(HSL, 100);
+  colorPool = new ColorPool();
+
+  colorPool.add(color(60, 100, 60));
+  colorPool.add(color(0, 100, 60));
+  colorPool.add(color(20, 100, 60));
+  colorPool.add(color(40, 100, 60));
+  colorPool.add(color(80, 100, 60));
+
+  console.log(colorPool.pool)
+  for (var i = 0; i < Math.floor(height / grid_size); i ++) {
+    for (var j = 0; j < Math.floor(width / grid_size); j ++) {
+      var randomAngle = Math.random() >= 0.5 ? PI / 4 : -PI / 4;
+
+      if (Math.random() > 0.5) {
+        fill(colorPool.random());
+        drawBar(j * grid_size, i * grid_size, grid_size / Math.cos(PI / 4) + bar_width, bar_width, PI / 4);
+      }
+      else {
+        fill(colorPool.random());
+        drawBar(j * grid_size, i * grid_size, grid_size / Math.cos(PI / 4) + bar_width, bar_width, -PI / 4);
+      }
+    }
+  }
+}
+
+function draw() {
+
+}
+
+function drawBar(cx, cy, w, h, angle) {
+  noStroke();
+  push();
+  translate(cx, cy);
+  rotate(angle);
+  rect(0, 0, w, h, 100, 100, 100, 100);
+  pop();
+}
+
+function ColorPool(colorMode, max) {
+  this.pool = [];
+
+  this.add = function(c) {
+    this.pool.push(c);
+  }
+
+  this.random = function() {
+    return this.pool[Math.floor(Math.random() * this.pool.length)]
+  }
+
+  this.select = function(n) {
+    return this.pool[n]
+  }
+}
